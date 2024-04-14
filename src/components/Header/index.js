80bf4cb1393a8a87773df63c3abf7d2497556055
Rohit-kumar-verma <rohitverma7754@gmail.com>
2024-04-14 17:09:59 +0000

    new code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,28 @@
-import {Link} from 'react-router-dom'
+import {useState} from 'react'
 import './index.css'
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div>
       <nav>
         <div className="navbar-container">
           <ul className="nav-items-block">
-            <Link to="/">
-              <li className="logo-name">
+            <li className="logo-name">
+              <a href="/">
                 COVID19<span>INDIA</span>
-              </li>
-            </Link>
-            <Link to="/">
-              <li className="nav-list">Home</li>
-            </Link>
-            <Link to="/about">
-              <li className="nav-list">About</li>
-            </Link>
+              </a>
+            </li>
+            <li className="nav-list">
+              <a href="/">Home </a>
+            </li>
+            <li className="nav-list">
+              <a href="/about">About</a>
+            </li>
           </ul>
         </div>
       </nav>
